116df920147dc4e85fe73fef05fc25d97569f78b
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-10-13 17:52:06 +0530

    Create ascii.C

---
+++ ascii.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+      int num=1;
+      while(num<=255)
+      {
+          printf("value:%dand ASCII character=%c\n",num,num);
+          num++;
+      }
+	return 0;
+}
